68e5c4f215f7dbbfa3673f60530468f0f3521802
Amit Singh <amitsingh@skycap.co.in>
2022-06-26 20:55:48 +0530

    [Added] Initial commit [Skipped] Few default models and Api Route file,

---
+++ lang/en/message.php
@@ -0,0 +1,8 @@
+<?php
+
+
+return [
+    "validation" => "Please correct all the validations",
+    "loginSuccess" => "User logged in successfully",
+    "invalidDetails" => "Invalid username or password",
+];
